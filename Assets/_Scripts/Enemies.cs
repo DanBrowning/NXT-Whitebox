a465cc2817f95059d9683320ba7d3c1b5a74281d
Dan <browning.dan@gmail.com>
2019-02-22 13:42:29 -0500

    Enemy Movement

---
+++ Assets/_Scripts/Enemies.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour {
+
+    public int nodeindex;
+    public Transform[] Paths;
+    
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (nodeindex < Paths.Length)
+        {
+            //Move our position a step closer to the target.
+            this.transform.position = Vector3.MoveTowards(this.transform.position, Paths[nodeindex].position, speed * Time.deltaTime);
+            this.transform.right = (Paths[nodeindex].position - this.transform.position).normalized;
+            //If we've reached the destination, move to the next one
+            if (this.transform.position == Paths[nodeindex].position)
+            {
+                nodeindex++;
+            }
+        }
+        else
+        {
+            nodeindex = 0;
+        }
+    }
+}
+
